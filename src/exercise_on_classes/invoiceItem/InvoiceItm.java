2987240c7b13a100d123413fdcb9091b8468031d
Saidakbar <nematullayevsaidakbar832@gmail.com>
2025-03-06 20:44:47 +0500

    first commit

---
+++ src/exercise_on_classes/invoiceItem/InvoiceItm.java
@@ -0,0 +1,59 @@
+package exercise_on_classes.invoiceItem;
+
+public class InvoiceItm {
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+    InvoiceItm(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    public double getTotal() {
+        return qty * unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItm{" +
+                "id=" + id +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
